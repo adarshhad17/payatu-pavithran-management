3f889f7f9e92735c7d8d82d6554da57cd275a8d3
Adarsh MP <adarshdq18@gmail.com>
2025-11-25 05:21:16 +0530

    Disable registration

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
 const r = express.Router();
-r.post("/register", register);
+// r.post("/register", register);
 r.post("/login", login);
 export default r;
